b1d4ed8c896015cc0b192ea818a0d8746eab12ae
jweste <julien.weste@akretion.com.br>
2016-05-01 00:29:06 +0200

    [IMP] repercute registration changes from template to shifts

--- coop_shift/model/shift_template_registration_line.py
+++ coop_shift/model/shift_template_registration_line.py
@@ -50,15 +50,88 @@ class ShiftTemplateRegistrationLine(models.Model):
 
     @api.model
     def create(self, vals):
-        res = super(ShiftTemplateRegistrationLine, self).create(vals)
         self._check_dates()
+        res = super(ShiftTemplateRegistrationLine, self).create(vals)
+
+        st_reg = self.env['shift.template.registration'].browse(
+            vals['registration_id'])
+        partner = st_reg.partner_id
+
+        begin = vals['date_begin']
+        end = vals['date_end']
+        shifts = st_reg.shift_template_id.shift_ids.filtered(
+            lambda s, b=begin, e=end: (s.date_begin > b or not b) and
+            (s.date_end < e or not e))
+
+        v = {
+            'partner_id': partner.id,
+            'state': vals['state']
+        }
+
+        sr_obj = self.env['shift.registration']
+        for shift in shifts:
+            ticket_id = shift.shift_ticket_ids.filtered(
+                lambda t: t.product_id == st_reg.shift_ticket_id.product_id)[0]
+            values = dict(v, **{
+                'shift_id': shift.id,
+                'shift_ticket_id': ticket_id.id,
+            })
+            sr_obj.create(values)
         return res
 
     @api.multi
     def write(self, vals):
-        res = super(ShiftTemplateRegistrationLine, self).write(vals)
+        sr_obj = self.env['shift.registration']
         self._check_dates()
+        res = super(ShiftTemplateRegistrationLine, self).write(vals)
+        st_reg = self.registration_id
+        partner = st_reg.partner_id
+
+        state = vals.get('state', self.state)
+        begin = vals.get('date_begin', self.date_begin)
+        end = vals.get('date_end', self.date_end)
+        # for shifts within dates: update registration if exists, or create it
+        shifts = st_reg.shift_template_id.shift_ids.filtered(
+            lambda s, b=begin, e=end: (s.date_begin > b or not b) and
+            (s.date_end < e or not e))
+        for shift in shifts:
+            found = False
+            for r in shift.registration_ids:
+                if r.partner_id == partner:
+                    r.state = state
+                    found = True
+            if not found:
+                ticket_id = shift.shift_ticket_ids.filtered(
+                    lambda t: t.product_id ==
+                    st_reg.shift_ticket_id.product_id)[0]
+                values = {
+                    'partner_id': partner.id,
+                    'state': state,
+                    'shift_id': shift.id,
+                    'shift_ticket_id': ticket_id.id,
+                }
+                sr_obj.create(values)
+
+        # for shifts not within dates: delete registration if exists
+        shifts = st_reg.shift_template_id.shift_ids.filtered(
+            lambda s, b=begin, e=end: (b and s.date_begin < b) or
+            (e and s.date_end > e))
+        for shift in shifts:
+            for r in shift.registration_ids:
+                if r.partner_id == partner:
+                    r.unlink()
+
         return res
+
+    @api.multi
+    def unlink(self):
+        for strl in self:
+            partner = strl.registration_id.partner_id
+            for shift in strl.registration_id.shift_template_id.shift_ids:
+                for r in shift.registration_ids:
+                    if r.partner_id == partner:
+                        r.unlink()
+        return super(ShiftTemplateRegistrationLine, self).unlink()
 
     @api.multi
     @api.constrains()
