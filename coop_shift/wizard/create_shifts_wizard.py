738d60a61fb83f72645d01125e748e54f9371ace
jweste <julien.weste@akretion.com.br>
2016-06-13 18:53:10 +0200

    [IMP] we can now select several templates in the list view and apply the create_shifts action on all of them

--- coop_shift/wizard/create_shifts_wizard.py
+++ coop_shift/wizard/create_shifts_wizard.py
@@ -32,28 +32,40 @@ class CreateShifts(models.TransientModel):
 
     @api.model
     def _get_last_shift_date(self):
-        template_id = self.env.context.get('active_id', False)
-        if template_id:
-            template = self.env['shift.template'].browse(template_id)
-            return template.last_shift_date
-        return False
+        if self.template_ids:
+            return min([t.last_shift_date for t in self.template_ids])
+        elif self.env.context.get('active_ids', False):
+            return min([
+                t.last_shift_date for t in self.env['shift.template'].browse(
+                    self.env.context['active_ids'])])
+        else:
+            return False
 
     @api.model
     def _get_default_date(self):
-        lsd = False
-        template_id = self.env.context.get('active_id', False)
-        if template_id:
-            template = self.env['shift.template'].browse(template_id)
-            lsd = template.last_shift_date
-            if lsd:
-                lsd = datetime.strptime(lsd, "%Y-%m-%d")
-                return datetime.strftime(
-                    lsd + timedelta(days=1), "%Y-%m-%d")
-            else:
-                return template.start_date
+        lsd = self.last_shift_date or self._get_last_shift_date()
+        if lsd:
+            lsd = datetime.strptime(lsd, "%Y-%m-%d")
+            return datetime.strftime(
+                lsd + timedelta(days=1), "%Y-%m-%d")
+        elif self.template_ids:
+            return min([t.start_date for t in self.template_ids])
         else:
             return datetime.now()
 
+    @api.model
+    def _get_selected_templates(self):
+        template_ids = self.env.context.get('active_ids', False)
+        if template_ids:
+            return template_ids
+        template_id = self.env.context.get('active_id', False)
+        if template_id:
+            return template_id
+        return []
+
+    template_ids = fields.Many2many(
+        'shift.template', 'template_createshift_rel', 'template_id',
+        'wizard_id', string="Templates", default=_get_selected_templates)
     last_shift_date = fields.Date(
         'Last created shift date', default=_get_last_shift_date)
     date_from = fields.Date(
@@ -66,66 +78,65 @@ class CreateShifts(models.TransientModel):
             if wizard.date_from <= wizard.last_shift_date:
                 raise UserError(_(
                     "'From date' can't be before 'Last shift date'"))
-            template_id = self.env.context.get('active_id', False)
-            if not template_id:
-                return
-            template = self.env['shift.template'].browse(template_id)
-            rec_dates = template.get_recurrent_dates(
-                after=self.date_from, before=self.date_to)
-            for rec_date in rec_dates:
-                rec_date = datetime(
-                    rec_date.year, rec_date.month, rec_date.day)
-                date_begin = datetime.strftime(
-                    rec_date + timedelta(hours=(template.start_time - 2)),
-                    "%Y-%m-%d %H:%M:%S")
-                date_end = datetime.strftime(
-                    rec_date + timedelta(hours=(template.end_time - 2)),
-                    "%Y-%m-%d %H:%M:%S")
-                rec_date = datetime.strftime(rec_date, "%Y-%m-%d")
-                vals = {
-                    'shift_template_id': template.id,
-                    'name': template.name,
-                    'user_id': template.user_id.id,
-                    'company_id': template.company_id.id,
-                    'seats_max': template.seats_max,
-                    'seats_availability': template.seats_availability,
-                    'seats_min': template.seats_min,
-                    'date_begin': date_begin,
-                    'date_end': date_end,
-                    'state': 'draft',
-                    'reply_to': template.reply_to,
-                    'address_id': template.address_id.id,
-                    'description': template.description,
-                    'shift_type_id': template.shift_type_id.id,
-                    'week_number': template.week_number,
-                    'shift_ticket_ids': None,
-                }
-                shift_id = self.env['shift.shift'].create(vals)
-                for ticket in template.shift_ticket_ids:
+            for template in wizard.template_ids:
+                rec_dates = template.get_recurrent_dates(
+                    after=self.date_from, before=self.date_to)
+                for rec_date in rec_dates:
+                    rec_date = datetime(
+                        rec_date.year, rec_date.month, rec_date.day)
+                    date_begin = datetime.strftime(
+                        rec_date + timedelta(hours=(template.start_time - 2)),
+                        "%Y-%m-%d %H:%M:%S")
+                    if date_begin.split(" ")[0] <= template.last_shift_date:
+                        continue
+                    date_end = datetime.strftime(
+                        rec_date + timedelta(hours=(template.end_time - 2)),
+                        "%Y-%m-%d %H:%M:%S")
+                    rec_date = datetime.strftime(rec_date, "%Y-%m-%d")
                     vals = {
-                        'name': ticket.name,
-                        'shift_id': shift_id.id,
-                        'product_id': ticket.product_id.id,
-                        'price': ticket.price,
-                        'deadline': ticket.deadline,
-                        'seats_availability': ticket.seats_availability,
-                        'seats_max': ticket.seats_max,
+                        'shift_template_id': template.id,
+                        'name': template.name,
+                        'user_id': template.user_id.id,
+                        'company_id': template.company_id.id,
+                        'seats_max': template.seats_max,
+                        'seats_availability': template.seats_availability,
+                        'seats_min': template.seats_min,
+                        'date_begin': date_begin,
+                        'date_end': date_end,
+                        'state': 'draft',
+                        'reply_to': template.reply_to,
+                        'address_id': template.address_id.id,
+                        'description': template.description,
+                        'shift_type_id': template.shift_type_id.id,
+                        'week_number': template.week_number,
+                        'shift_ticket_ids': None,
                     }
-                    ticket_id = self.env['shift.ticket'].create(vals)
+                    shift_id = self.env['shift.shift'].create(vals)
+                    for ticket in template.shift_ticket_ids:
+                        vals = {
+                            'name': ticket.name,
+                            'shift_id': shift_id.id,
+                            'product_id': ticket.product_id.id,
+                            'price': ticket.price,
+                            'deadline': ticket.deadline,
+                            'seats_availability': ticket.seats_availability,
+                            'seats_max': ticket.seats_max,
+                        }
+                        ticket_id = self.env['shift.ticket'].create(vals)
 
-                    for attendee in ticket.registration_ids:
-                        state, strl_id = attendee._get_state(rec_date)
-                        if state:
-                            vals = {
-                                'partner_id': attendee.partner_id.id,
-                                'user_id': template.user_id.id,
-                                'state': state,
-                                'email': attendee.email,
-                                'phone': attendee.phone,
-                                'name': attendee.name,
-                                'shift_id': shift_id.id,
-                                'shift_ticket_id': ticket_id.id,
-                                'tmpl_reg_line_id': strl_id,
-                                'template_created': True,
-                            }
-                            self.env['shift.registration'].create(vals)
+                        for attendee in ticket.registration_ids:
+                            state, strl_id = attendee._get_state(rec_date)
+                            if state:
+                                vals = {
+                                    'partner_id': attendee.partner_id.id,
+                                    'user_id': template.user_id.id,
+                                    'state': state,
+                                    'email': attendee.email,
+                                    'phone': attendee.phone,
+                                    'name': attendee.name,
+                                    'shift_id': shift_id.id,
+                                    'shift_ticket_id': ticket_id.id,
+                                    'tmpl_reg_line_id': strl_id,
+                                    'template_created': True,
+                                }
+                                self.env['shift.registration'].create(vals)
